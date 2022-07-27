d8ebd5051b891850b8a95772e75c04b6a8b39f44
Taras Oleksyshyn <x3mal18@gmail.com>
2022-07-27 15:31:43 +0300

    check values for dynamic information

--- src/js/main.js
+++ src/js/main.js
@@ -351,6 +351,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const input = document.querySelector(selector);
 
     input.addEventListener('input', () => {
+
+      if (input.value.match(/\D/g)) {
+        input.style.border = '2px solid red';
+        input.style.color = 'red';
+      } else {
+        input.style.border = '';
+        input.style.color = '';
+      }
+
       switch (input.getAttribute('id')) {
         case 'height':
           height = +input.value;
