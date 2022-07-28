1427024fb72758a8dce78b8b2b64a5f53205cabb
Taras Oleksyshyn <x3mal18@gmail.com>
2022-07-28 11:05:00 +0300

    modals module was created

---
+++ src/js/modules/modal.js
@@ -0,0 +1,33 @@
+function modal() {
+  const modalBtns = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalTimerId = setTimeout(openModal, 50000);
+
+  function openModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+    clearInterval(modalTimerId);
+  }
+  function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+  }
+
+  modalBtns.forEach(btn => {
+    btn.addEventListener('click', openModal);
+  });
+
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal || e.target.getAttribute('data-close') == '') {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape' && modal.classList.contains('show')) { closeModal(); }
+  });
+}
+
+module.exports = modal;
