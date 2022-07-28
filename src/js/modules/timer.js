3b4742b51afe5dbab962e6d3bd689fe2353188dc
Taras Oleksyshyn <x3mal18@gmail.com>
2022-07-28 11:05:26 +0300

    timer module was created

---
+++ src/js/modules/timer.js
@@ -0,0 +1,64 @@
+function timer() {
+  const deadline = '2022-08-01';
+  const promotionDate = document.querySelector('.promotion__date');
+  promotionDate.innerText = deadline.split('-').reverse().join('.');
+
+  function getTimeRemining(endtime) {
+    let days, hours, minutes, seconds;
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    if (total < 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      days = Math.floor(total / (1000 * 60 * 60 * 24));
+      hours = Math.floor(total / (1000 * 60 * 60) % 24);
+      minutes = Math.floor((total / 1000 / 60) % 60);
+      seconds = Math.floor((total / 1000) % 60);
+    }
+
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num <= 9) {
+      return `0${num}`;
+    }
+    return num;
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector('#days'),
+      hours = timer.querySelector('#hours'),
+      minutes = timer.querySelector('#minutes'),
+      seconds = timer.querySelector('#seconds'),
+      timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeRemining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+  setClock('.timer', deadline);
+}
+
+module.exports = timer;
+
