243eb5ac00f7b7aae13fff51d0bc5040d1729d33
Taras Oleksyshyn <x3mal18@gmail.com>
2022-07-28 11:04:39 +0300

    forms module was created

---
+++ src/js/modules/forms.js
@@ -0,0 +1,82 @@
+function forms() {
+  const forms = document.querySelectorAll('form');
+  const messages = {
+    loading: 'img/form/spinner.svg',
+    success: 'Все ок! Скоро ми з вами звяжемось',
+    failure: 'Упс! Щось пішло не так',
+  };
+
+  forms.forEach(item => {
+    bindPostData(item);
+  });
+
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: data
+    });
+    return await res.json();
+  };
+
+  async function getResource(url) {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status ${res.status}`);
+    }
+    return await res.json();
+  }
+
+  function bindPostData(form) {
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      const statusMessage = document.createElement('img');
+      statusMessage.src = messages.loading;
+      statusMessage.classList.add('status');
+      form.insertAdjacentElement('afterend', statusMessage);
+
+      const formData = new FormData(form);
+
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+      postData('http://localhost:3000/requests', json)
+        .then(data => {
+          showThanksModal(messages.success);
+          statusMessage.remove();
+        }).catch(() => {
+          showThanksModal(messages.failure);
+        }).finally(() => {
+          form.reset();
+        });
+    });
+  }
+
+  function showThanksModal(message) {
+    const prevModalDialog = document.querySelector('.modal__dialog');
+
+    prevModalDialog.classList.add('hide');
+    openModal();
+
+    const thanksModal = document.createElement('div');
+    thanksModal.classList.add('modal__dialog');
+    thanksModal.innerHTML = `
+    <div class="modal__content">
+      <div data-close class="modal__close">&times;</div>
+      <div class="modal__title">${message}</div>
+    </div>
+    `;
+
+    document.querySelector('.modal').append(thanksModal);
+    setTimeout(() => {
+      thanksModal.remove();
+      prevModalDialog.classList.add('show');
+      prevModalDialog.classList.remove('hide');
+      closeModal();
+    }, 4000);
+  }
+}
+
+module.exports = forms;
